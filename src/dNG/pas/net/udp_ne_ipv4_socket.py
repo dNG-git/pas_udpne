644fff46efd2c6c6f123b8397b92459d585b203a
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:30:21 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/pas/net/udp_ne_ipv4_socket.py
+++ src/dNG/pas/net/udp_ne_ipv4_socket.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;udpne
+https://www.direct-netware.de/redirect?pas;udpne
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasUdpNEVersion)#
 #echo(__FILEPATH__)#
@@ -31,7 +31,7 @@ multicast packets over IPV4.
 :package:    pas
 :subpackage: udpne
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
